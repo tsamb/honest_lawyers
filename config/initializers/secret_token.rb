117884ab3eeb0f8c8d97eb7d7d3db07af8de78d7
stevenspiel <stevenspiel@gmail.com>
2014-02-19 17:39:36 -0600

    first commit, rails 4 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HonestLawyers::Application.config.secret_key_base = 'fa4a3caf1fe7d494e94325c2189dc4ca0c5a2dba3ffef852e32f5db81a3466f92c1bcbf31a6273b6677ac0b2f0e38e12ab7d3d0e5083a851b6d6cf1b0f335e0b'
