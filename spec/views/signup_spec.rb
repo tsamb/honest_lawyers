c4694d18c8a3051c75c46906bf458287b760fa1a
JuLu <junting.lu@gmail.com>
2014-02-20 11:20:33 -0600

    uploading to github in order to pair on this with sam

--- spec/views/signup_spec.rb
+++ spec/views/signup_spec.rb
@@ -2,14 +2,33 @@ require_relative '../spec_helper'
 
 describe 'users/new.html.erb' do
 
+  before { visit new_user_path }
+
   it 'should be successful' do
-    visit new_user_path # not sure if this is the right path, but whatever
     response.should be_success
   end
 
   it 'should have form' do
-    get new_user_path
-
     page.should have_css('form')
   end
-end
+
+  describe "with invalid information" do
+    it 'should not allow submission of blank signup form' do
+      expect { click_button "Signup" }.not_to change(User, :count)
+    end
+  end
+
+  describe "wtih valid information" do
+    before do
+        fill_in "Name",         with: "Ju Lu"
+        fill_in "Email",        with: "abc@abc.com"
+        fill_in "Password",     with: "password"
+        fill_in "Confirmation", with: "password"
+      end
+
+    it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+    end
+  end
+
+end #End main describe
