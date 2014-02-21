8c846f22bb4b6430944cd2740e8f2f5794e3c2d9
Sam and Ben <sblackman@gmail.com>
2014-02-21 15:25:12 -0600

    plug together changes throughout the whole project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_task
 
   def time_formatter(seconds)
     hours = seconds / 1.hour
@@ -12,8 +12,16 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    User.find(session[:user_id])
-    # User.find(1)
+    User.find_by(id: session[:user_id])
+  end
+
+  def current_task
+    user = current_user
+    if user
+      user.current_task
+    else
+      nil
+    end
   end
 
   def logged_in?
